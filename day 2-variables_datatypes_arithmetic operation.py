f5e2d6889ed59594d53f62d78c918f3b83223bfe
Rakshitha C.S <rakshithacs887@gmail.com>
2026-06-24 20:51:36 +0530

    Add files via upload
    Day 3 of my Python Learning Journey 🚀
    
    Today I practiced:
    ✅ Variables
    ✅ User Input and Output
    ✅ Arithmetic Operations
    ✅ Swapping Variables
    
    I also uploaded my practice programs to GitHub and am building my coding portfolio step by step.
    
    Every small step counts. Looking forward to learning more Python and building projects in the coming days. 💻
    
    #Python #Coding #Programming #GitHub #ComputerScience #LearningJourney

---
+++ day 2-variables_datatypes_arithmetic operation.py
@@ -0,0 +1,21 @@
+name="rakshitha"
+age=18
+dream_job="software engineer"
+
+print("name:",name)
+print("age:",age)
+print("dream job:",dream_job)
+
+a=15
+b=25
+
+print("arithmetic operations")
+print("addition=",a+b)
+print("subtraction=",a-b)
+print("multiplication=",a*b)
+print("division=",a/b)
+
+a,b=b,a
+print("after swapping")
+print("a=",a)
+print("b=",b)
